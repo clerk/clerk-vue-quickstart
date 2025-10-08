46151dfe9d75dd6170fb3ba7212f7228297351e3
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2025-10-08 19:03:20 -0400

    bump

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <script setup lang="ts">
-import {
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/vue';
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/vue'
 </script>
 
 <template>
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+  <header>
+    <SignedOut>
+      <SignInButton />
+    </SignedOut>
+    <SignedIn>
+      <UserButton />
+    </SignedIn>
+  </header>
 </template>
